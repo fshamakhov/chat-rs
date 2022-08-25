944dc21dbb358c27d5a65dea8023fa320a72abca
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2022-08-25 20:06:09 +0800

    fix: 🔌  Symmetric connection between peers

--- src/lib.rs
+++ src/lib.rs
@@ -3,127 +3,13 @@ use ockam::{
     identity::{Identity, TrustEveryonePolicy},
     route,
     vault::Vault,
-    Address, Context, Result, Route, Routed, TcpTransport, Worker, TCP,
+    Context, Result, Routed, TcpTransport, Worker, TCP,
 };
 use std::{
     error::Error,
     io::{stdin, stdout, Write},
+    time::Duration,
 };
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum Mode {
-    Server,
-    Client,
-}
-
-impl clap::ValueEnum for Mode {
-    fn value_variants<'a>() -> &'a [Self] {
-        &[Self::Client, Self::Server]
-    }
-
-    fn to_possible_value<'a>(&self) -> Option<clap::PossibleValue<'a>> {
-        match self {
-            Self::Client => Some(clap::PossibleValue::new("client")),
-            Self::Server => Some(clap::PossibleValue::new("server")),
-        }
-    }
-}
-
-pub struct ClientWorker;
-
-#[ockam::worker]
-impl Worker for ClientWorker {
-    type Context = Context;
-    type Message = String;
-
-    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<String>) -> Result<()> {
-        let return_route = msg.return_route().clone();
-        ctx.send(return_route, "Ok".to_string()).await?;
-        println!();
-        println!("chat: {}", msg.body());
-        print!("message: ");
-        stdout().flush().expect("Stdout error");
-        Ok(())
-    }
-}
-
-pub struct ServerWorker {
-    started_sender: bool,
-    parent_address: Address,
-}
-
-impl Default for ServerWorker {
-    fn default() -> Self {
-        ServerWorker {
-            started_sender: false,
-            parent_address: Address::random_local(),
-        }
-    }
-}
-
-pub struct ServerSenderWorker {
-    route: Route,
-    parent_address: Address,
-}
-
-#[ockam::worker]
-impl Worker for ServerSenderWorker {
-    type Context = Context;
-    type Message = String;
-
-    async fn initialize(&mut self, ctx: &mut Self::Context) -> Result<()> {
-        println!("Type a message and hit Enter to send it");
-        loop {
-            print!("message: ");
-            stdout().flush().expect("Stdout error");
-            let msg = get_input();
-
-            if msg == ":quit" {
-                self.shutdown(ctx).await?;
-                ctx.send(route![self.parent_address.clone()], ":quit".to_string())
-                    .await?;
-                break;
-            } else {
-                ctx.send(self.route.clone(), msg).await?;
-                ctx.receive::<String>().await?;
-            }
-        }
-        Ok(())
-    }
-}
-
-#[ockam::worker]
-impl Worker for ServerWorker {
-    type Context = Context;
-    type Message = String;
-
-    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<String>) -> Result<()> {
-        let return_route = msg.return_route().clone();
-        ctx.send(return_route, "Ok".to_string()).await?;
-
-        if !self.started_sender {
-            self.started_sender = true;
-            let mut message = msg.into_local_message();
-            let transport_message = message.transport_mut();
-
-            let address = transport_message.return_route.next()?;
-            ctx.start_worker(
-                "server_sender_worker",
-                ServerSenderWorker {
-                    parent_address: self.parent_address.clone(),
-                    route: route![address.clone(), "client_worker"],
-                },
-            )
-            .await?;
-        } else {
-            println!();
-            println!("chat: {}", msg.body());
-            print!("message: ");
-            stdout().flush().expect("Stdout error");
-        }
-        Ok(())
-    }
-}
 
 fn get_input() -> String {
     let mut buff = String::new();
@@ -134,16 +20,33 @@ fn get_input() -> String {
 
     buff.trim().to_string()
 }
+struct ChatWorker;
 
-pub async fn start_server(host: &str, port: &str, mut ctx: Context) -> Result<(), Box<dyn Error>> {
-    ctx.start_worker(
-        "server_worker",
-        ServerWorker {
-            parent_address: ctx.address(),
-            ..Default::default()
-        },
-    )
-    .await?;
+#[ockam::worker]
+impl Worker for ChatWorker {
+    type Context = Context;
+    type Message = String;
+
+    async fn handle_message(&mut self, ctx: &mut Context, msg: Routed<String>) -> Result<()> {
+        let return_route = msg.return_route().clone();
+        ctx.send(return_route, "Ok".to_string()).await?;
+
+        println!();
+        println!("chat: {}", msg.body());
+        print!("message: ");
+        stdout().flush().expect("Stdout error");
+
+        Ok(())
+    }
+}
+
+pub async fn run(
+    host: &str,
+    port: &str,
+    port_connect: &str,
+    mut ctx: Context,
+) -> Result<(), Box<dyn Error>> {
+    ctx.start_worker("worker", ChatWorker).await?;
 
     let tcp = TcpTransport::create(&ctx).await?;
 
@@ -156,65 +59,38 @@ pub async fn start_server(host: &str, port: &str, mut ctx: Context) -> Result<()
     let storage = InMemoryStorage::new();
 
     identity
-        .create_secure_channel_listener("server", TrustEveryonePolicy, &storage)
+        .create_secure_channel_listener("chat", TrustEveryonePolicy, &storage)
         .await?;
 
-    println!("Started server on port {}", port);
-    println!("Waiting for chat mate...");
+    println!("Waiting for chat mate");
 
-    let reply = ctx.receive::<String>().await?;
-    if reply == ":quit".to_string() {
-        println!("Bye Bye!");
-        ctx.stop_worker("server_worker").await?;
-        ctx.stop().await?;
-    }
+    'outer: loop {
+        if let Ok(channel) = identity
+            .create_secure_channel_extended(
+                route![(TCP, &format!("{host}:{port_connect}")), "chat"],
+                TrustEveryonePolicy,
+                &storage,
+                Duration::from_secs(1),
+            )
+            .await
+        {
+            println!("Type a message and hit Enter to send it");
+            println!("To quit type :quit and hit Enter");
+            loop {
+                print!("message: ");
+                stdout().flush()?;
+                let msg = get_input();
 
-    Ok(())
-}
-
-pub async fn connect_to_server(
-    host: &str,
-    port: &str,
-    mut ctx: Context,
-) -> Result<(), Box<dyn Error>> {
-    TcpTransport::create(&ctx).await?;
-
-    let vault = Vault::create();
-
-    let identity = Identity::create(&ctx, &vault).await?;
-
-    let storage = InMemoryStorage::new();
-
-    let route = route![(TCP, format!("{host}:{port}")), "server"];
-
-    ctx.start_worker("client_worker", ClientWorker).await?;
-
-    let channel = identity
-        .create_secure_channel(route, TrustEveryonePolicy, &storage)
-        .await?;
-
-    println!("Successfully connected to chat at {host}:{port}");
-
-    let route = route![channel.clone(), "server_worker"];
-
-    ctx.send(route.clone(), "Hello Ockam!".to_string()).await?;
-
-    ctx.receive::<String>().await?;
-
-    println!("Type a message and hit Enter to send it");
-    loop {
-        print!("message: ");
-        stdout().flush()?;
-        let msg = get_input();
-
-        if msg == ":quit" {
-            println!("Bye Bye!");
-            ctx.stop_worker("client_worker").await?;
-            ctx.stop().await?;
-            break;
-        } else {
-            ctx.send(route.clone(), msg).await?;
-            ctx.receive::<String>().await?;
+                if msg == ":quit" {
+                    println!("Bye Bye!");
+                    ctx.stop_worker("worker").await?;
+                    ctx.stop().await?;
+                    break 'outer;
+                } else {
+                    ctx.send_and_receive(route![channel.clone(), "worker"], msg)
+                        .await?;
+                }
+            }
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,43 @@
-use std::{process};
+use std::process;
 
-use chat::{self, Mode};
+use chat;
 use clap::{App, Arg};
 use ockam::Context;
-
 
 #[ockam::node]
 async fn main(ctx: Context) {
     let matches = App::new("P2P Chat")
         .version("0.1.0")
-        .about("Simple p2p chat written in Rust")
-        .arg(
-            Arg::new("mode")
-                .short('m')
-                .long("mode")
-                .value_parser(clap::builder::EnumValueParser::<Mode>::new())
-                .default_value("server"),
-        )
+        .about("Simple p2p chat written in Rust with encrypted channel")
         .arg(
             Arg::new("host")
                 .short('h')
                 .long("host")
                 .default_value("127.0.0.1")
-                .help("Host to connect to in client mode or address to bind to in server mode"),
+                .help("IP address to connect to"),
         )
         .arg(
             Arg::new("port")
                 .short('p')
                 .long("port")
                 .default_value("6000")
-                .help("Host to connect to in client mode or address to bind to in server mode"),
+                .help("Port to listen to"),
+        )
+        .arg(
+            Arg::new("port_connect")
+                .short('c')
+                .long("port_connect")
+                .default_value("6000")
+                .help("Port to connect to"),
         )
         .get_matches();
 
-    let mode: &Mode = matches.get_one("mode").unwrap();
     let host: &String = matches.get_one("host").unwrap();
     let port: &String = matches.get_one("port").unwrap();
+    let port_connect: &String = matches.get_one("port_connect").unwrap();
 
-    if let Err(e) = match mode {
-        Mode::Server => chat::start_server(host, port, ctx).await,
-        Mode::Client => chat::connect_to_server(host, port, ctx).await,
-    } {
+    if let Err(e) = chat::run(host, port, port_connect, ctx).await {
         eprintln!("Application error: {e}");
         process::exit(1);
-
     }
 }
